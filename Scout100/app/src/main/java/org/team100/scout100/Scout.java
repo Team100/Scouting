82ebff1f4b5e76805834602f365d02a36bdb27e4
RyanSearcy <rvsearcy1@gmail.com>
2015-02-01 11:27:59 -0800

    ryan's app code

---
+++ Scout100/app/src/main/java/org/team100/scout100/Scout.java
@@ -0,0 +1,63 @@
+package org.team100.scout100;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class Scout extends ActionBarActivity {
+
+
+    private EditText newTextBox;
+    public void SaveData (View view) {
+        EditText newTextBox = (EditText) findViewById(R.id.newTextBox);
+        Toast toast = new Toast(getApplicationContext());
+        toast.makeText(Scout.this, newTextBox.getText(),Toast.LENGTH_SHORT).show();
+    }
+   public File GetDocStorage(String docName) {
+    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), docName);
+        if (!file.mkdirs()) {
+            Log.e(LOG_TAG, "Directory not created");
+        }
+        return file;
+    }
+    private static final String LOG_TAG = "SaveData";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scout);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
